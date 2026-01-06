1ec9863169b9c3220868f61ea2f4048874754794
MD.Naim-UL-Haque <naimulh644@gmail.com>
2026-01-07 00:36:39 +0600

    Postgres

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)         
+
+class Project(models.Model):
+    name = models.CharField( max_length=150)
+    start_date = models.DateField()
+
+class Task(models.Model) :
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,      #many to one
+        default=1
+        ) # jodi project niche thakto tahole " " qutaion er majhe likte hoito project
+    assigned_to = models.ManyToManyField(Employee , related_name='tasks')   
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    due_date = models.DateField()
+    is_completed = models.BooleanField(default = False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField( auto_now=True)
+
+#One to One 
+#Many to one
+#many to many
+
+class TaskDetail(models.Model):
+    HIGH = 'H'
+    MEDIUM = 'M'
+    LOW = 'L'
+    PRIORITY_OPTIONS = (
+        (HIGH,'High'),
+        (MEDIUM,'Medium'),
+        (LOW,'Low')
+    )
+    
+    task = models.OneToOneField(
+        Task,
+        on_delete=models.CASCADE,
+        related_name = 'details'
+        )
+    assigned_to = models.CharField(max_length=100)
+    priority = models.CharField( max_length=1 , choices=PRIORITY_OPTIONS , default=LOW)
+    
+
+
+# Task.objects.get(id=2)
+#select * from task where id = 2
+# ORM = Object relational Mapper  
+
+#many to many 
+
+#task = many employee 1 task 
+# employee = many tasks 1 employee 
+
--- tasks/views.py
+++ tasks/views.py
@@ -1,26 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def home(request) :
-      # work with data-base 
-      # transform data
-      # data pass 
-      # HTTP response / Json response 
-    return HttpResponse("Welcome tho the task management system")
+# def home(request) :
+#       # work with data-base 
+#       # transform data
+#       # data pass 
+#       # HTTP response / Json response 
+#     return HttpResponse("Welcome tho the task management system")
 
-def contact(request):
-    return HttpResponse("<h1 style = 'color : red '>This is contact page</h1>")
+# def contact(request):
+#     return HttpResponse("<h1 style = 'color : red '>This is contact page</h1>")
 
-def show_task(request) :
-    return HttpResponse("<h1>This is our task page</h1>")
-def show_specific_task(request,id):
-    print("id : ",id)
-    print("id type : ", type(id) )
-    return HttpResponse(f"<h1> This is specific task page {id} </h1>")
+# def show_task(request) :
+#     return HttpResponse("<h1>This is our task page</h1>")
+# def show_specific_task(request,id):
+#     print("id : ",id)
+#     print("id type : ", type(id) )
+#     return HttpResponse(f"<h1> This is specific task page {id} </h1>")
 
-def dashboard(request,id):
-    return HttpResponse ("This is Dashboard")
+# def dashboard(request,id):
+#     return HttpResponse ("This is Dashboard")
 
 
-def show_admin(request):
-    return HttpResponse("This is admin")
+# def show_admin(request):
+#     return HttpResponse("This is admin")
+
+def manager_dashboard(request):
+    return render(request,"dashboard/manager_dashboard.html")
+def user_dashboard(request):
+    return render(request,"dashboard/user_dashboard.html")
+def test(request):
+    context = {
+        "names" : ["Mahmud","Ahmed","Jhon","Mr.X"],
+         "age" : 23,
+            
+    }
+    return render(request,"test.html",context)
